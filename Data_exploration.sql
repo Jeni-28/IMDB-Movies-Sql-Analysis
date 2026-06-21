85da9c383dbf800ae7d13d91b3fe18405c57b478
Jenihelan M <153791476+Jeni-28@users.noreply.github.com>
2026-06-21 22:41:18 +0530

    Add files via upload

---
+++ Data_exploration.sql
@@ -0,0 +1,3 @@
+USE project_movie_database;
+SELECT * FROM movies LIMIT 10;
+SELECT * FROM directors LIMIT 10;
---
+++ Database_overview.sql
@@ -0,0 +1,4 @@
+USE project_movie_database;
+SHOW TABLES;
+describe movies;
+describe directors;
